53fcdf9a7dd46c7d11417de4d351b7898386b869
dailybacon <themutie@gmail.com>
2025-01-01 22:21:28 -0600

    added readme, circleshape.py

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Using pygame, this is the game known by some as "Asteroids", named after the eponymous space rock.  Also, this is the second [Boot.dev](https://www.boot.dev) guided project.  Hooray!
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 
 def main():
 	pygame.init()
+	time_clock = pygame.time.Clock()
+	dt = 0
 	print("Starting asteroids!")
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
@@ -18,6 +20,7 @@ def main():
 				return
 		screen.fill('black')
 		pygame.display.flip()
+		dt = time_clock.tick() / 1000
 
 if __name__ == "__main__":
 	main()
