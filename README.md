a534a6ac42b5177aca3ee3d754d22e405fd64877
dailybacon <themutie@gmail.com>
2025-01-02 22:50:40 -0600

    added player ship/triangle

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # README
 
-Using pygame, this is the game known by some as "Asteroids", named after the eponymous space rock.  Also, this is the second [Boot.dev](https://www.boot.dev) guided project.  Hooray!
+Using pygame, this is the game known by some as "Asteroids", named after the eponymous space rock.  Also, this is the second [boot.dev](https://www.boot.dev) guided project.  Hooray!
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -13,14 +14,19 @@ def main():
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill('black')
+
+		player.draw(screen)
+		player.update(dt)
 		pygame.display.flip()
-		dt = time_clock.tick() / 1000
+		dt = (time_clock.tick() / 1000)
+
 
 if __name__ == "__main__":
 	main()
