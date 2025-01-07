54bad3c64a46ae186f13035843f2a62996d1093f
dailybacon <themutie@gmail.com>
2025-01-07 01:58:19 -0600

    finished game

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
+import sys
 
 
 def main():
@@ -14,16 +18,49 @@ def main():
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = updatable
+	asteroidfield = AsteroidField()
 
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
 		screen.fill('black')
 
-		player.draw(screen)
-		player.update(dt)
+		for thing in updatable:
+			thing.update(dt)
+		
+		for asteroid in asteroids:
+			for shot in shots:
+				if shot.collide(asteroid):
+					shot.kill()
+					asteroid.split()
+
+		for thing in drawable:
+			thing.draw(screen)
+
+			
+			
+
+
+
+		for asteroid in asteroids:
+			if(player.collide(asteroid)):
+				print("Game Over!")
+				sys.exit()
+
 		pygame.display.flip()
 		dt = (time_clock.tick() / 1000)
 
